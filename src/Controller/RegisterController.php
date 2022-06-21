2d235e141c446fff008016fa1f87b5094c21a8cb
ceyssat <ceyssat.franck@gmail.com>
2022-06-21 16:01:21 +0200

    formulaire inscription ok bddok

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,15 +2,67 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegisterController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
-    public function index(): Response
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entitymanager)
     {
-        return $this->render('register/index.html.twig');
+        $this->entityManager = $entitymanager;
+    }
+    #[Route('/register ', name: 'app_register')]
+    public function index(Request $request, UserPasswordHasherInterface $encoder)
+    {
+        $notification = null;
+
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getdata();
+            //verifiie les mdp ci dessous/
+            $search_mail = $this->entityManager->getRepository(User::class)
+            ->findOneByEmail($user->getEmail());
+            $password = $encoder->hashPassword($user, $user->getPassword());
+               $user->setPassword($password);
+               // si l email n est apas present en base de donnes tu peux continuer (envouyer a la bdd) si c est pas le cas notification
+               if (!$search_mail){
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+         
+           
+            $notification = "Votre inscription s'est correctement déroulée.Veuillez vous
+            connecter à votre compte";
+               }else{
+                $notification = "L'email existe déja.Merci d'essayer avec un autre email";
+               }
+         }
+        //conditions si le formulaire remplis tu renvois sur la page connection
+        if ($this->getUser()){
+           
+            return $this->redirectToRoute('app_login');
+        }
+        
+        return $this->render('register/index.html.twig', [
+
+            'form' => $form->createView(),
+            'notification' => $notification
+        ]);
+
+        
+        
     }
 }
+
