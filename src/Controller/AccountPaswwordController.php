2c6894fca989936abd8c754ee76535c969f20753
ceyssat <ceyssat.franck@gmail.com>
2022-06-21 18:46:28 +0200

    accountPasswordController

--- src/Controller/AccountPaswwordController.php
+++ src/Controller/AccountPaswwordController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AccountController extends AbstractController
+class AccountPaswwordController extends AbstractController
 {
-    #[Route('/compte', name: 'app_account')]
+    #[Route('/compte/modifie-mon-mot-de-passe', name: 'app_account_paswword')]
     public function index(): Response
     {
-        return $this->render('account/index.html.twig');
+        return $this->render('account/pasword.html.twig');
     }
 }
