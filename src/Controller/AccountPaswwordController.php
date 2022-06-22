bca9029139362682d718e112ba30c042091e60aa
ceyssat <ceyssat.franck@gmail.com>
2022-06-22 07:49:17 +0200

    form change password ok

--- src/Controller/AccountPaswwordController.php
+++ src/Controller/AccountPaswwordController.php
@@ -2,15 +2,52 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ChangePasswordType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AccountPaswwordController extends AbstractController
 {
-    #[Route('/compte/modifie-mon-mot-de-passe', name: 'app_account_password')]
-    public function index(): Response
+    //rajoute le parametre entitymanager pour permmttre d envoyer plus bas le form a la bdd
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        return $this->render('account/password.html.twig');
+        $this->entityManager = $entityManager;
+    }
+    #[Route('/compte/modifie-mon-mot-de-passe', name: 'app_account_password')]
+    public function index(Request $request, UserPasswordHasherInterface $encoder)
+    {
+        $notification = null;
+
+        $user = $this->getUser();
+        $form = $this->createForm(ChangePasswordType::class, $user);
+        
+        $form->handleRequest($request);
+        // conditions si le form est bien remplis et valid alors
+        if ($form->isSubmitted() && $form->isValid()){
+            $old_pwd = $form->get('old_password')->getData();
+          
+            if ($encoder->isPasswordValid($user, $old_pwd)){
+                $new_pwd = $form->get('new_password')->getData();
+                $password = $encoder->hashPassword($user,$new_pwd);
+                
+                $user->setPassword($password);
+            //on envoie a la bdd
+            $this->entityManager->flush();
+            $notification = 'Votre mot de passe a bien été mis à jour.';
+
+            } else {
+                $notification = "Votre mot de passe actuel n'est pas le bon";
+            }
+        }
+        return $this->render('account/password.html.twig',[
+            'form' => $form->createView(),
+            'notification' => $notification
+        ]);
     }
 }
