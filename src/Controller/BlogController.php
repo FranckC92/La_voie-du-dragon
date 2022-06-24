d3402201062dc451a42ce34f22f02b0e672fabad
ceyssat <ceyssat.franck@gmail.com>
2022-06-24 16:10:06 +0200

    route ok show blog avec image interractive soucis navbar comprends pas

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Blog;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,8 +19,8 @@ class BlogController extends AbstractController
         $this->entityManager = $entityManager;
     }
  
-    #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+    #[Route('/les-blogs', name: 'app_blogs')]
+    public function index(Request $request): Response
     {
 
         $blogs = $this->entityManager->getRepository(Blog::class)->findAll();
@@ -27,7 +28,19 @@ class BlogController extends AbstractController
         
         return $this->render('blog/index.html.twig', [
             'blogs' => $blogs,
-           
+            ]);
+    }
+    #[Route('/mon-blog/{slug}', name: 'app_blog')]
+    public function show($slug): Response
+    {
+
+        $blog = $this->entityManager->getRepository(Blog::class)->findOneBySlug($slug);
+
+       
+ 
+        return $this->render('blog/show.html.twig', [
+            'blog' => $blog,
+            
         ]);
     }
 }
