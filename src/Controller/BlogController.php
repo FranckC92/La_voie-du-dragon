f1063f7a6f137f38fd1311888c5c5c18e46a6ecb
ceyssat <ceyssat.franck@gmail.com>
2022-06-24 14:44:20 +0200

    Blog controller ok twig ok

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,15 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Blog;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
+    //utilise doctrine pour recuper mes objets
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+ 
     #[Route('/blog', name: 'app_blog')]
     public function index(): Response
     {
-        return $this->render('blog/index.html.twig');
+
+        $blogs = $this->entityManager->getRepository(Blog::class)->findAll();
+
+        
+        return $this->render('blog/index.html.twig', [
+            'blogs' => $blogs,
+           
+        ]);
     }
 }
