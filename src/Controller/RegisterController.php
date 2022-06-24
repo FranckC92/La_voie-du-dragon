92e9953d51eafbecc023b0778df436f1b0732d73
ceyssat <ceyssat.franck@gmail.com>
2022-06-24 11:50:39 +0200

    message et mail inscription ok

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Classe\Mail;
 use App\Form\RegisterType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -41,7 +42,7 @@ class RegisterController extends AbstractController
             $this->entityManager->persist($user);
             $this->entityManager->flush();
 
-         
+            $mail = new Mail();
            
             $notification = "Votre inscription s'est correctement déroulée.Veuillez vous
             connecter à votre compte";
