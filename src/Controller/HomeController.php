339235a59a65ca48061835a8f4b1e93338f8f483
ceyssat <ceyssat.franck@gmail.com>
2022-07-03 09:24:41 +0200

    mis en place baniere cookies

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
-use App\Classe\Mail;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        //créations cookies avec propriété nom/valeur/expiration/path(home)/domaine/secu hhtps & http
+        $cookie = new Cookie('couleur',
+                            'rouge',
+                            strtotime('tomorrow'),
+                            '/',
+                            'digitaldev.fr',
+                            true,
+                            true);
+                         
+
         return $this->render('home/index.html.twig');
     }
 }
