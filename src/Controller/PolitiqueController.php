f511d6ac6f7fa059ce561615f424d5479e072092
ceyssat <ceyssat.franck@gmail.com>
2022-07-06 10:07:17 +0200

    controller + vue politique grpd

---
+++ src/Controller/PolitiqueController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PolitiqueController extends AbstractController
+{
+    #[Route('/politique/confidentialité', name: 'app_politique')]
+    public function index(): Response
+    {
+        return $this->render('politique/index.html.twig');
+    }
+}
