c5471c606a17e9c9acb628b95e549749f7804709
ceyssat <ceyssat.franck@gmail.com>
2022-08-26 16:33:25 +0200

    contact formulaire ok vider et message

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use App\Form\ContactType;
 use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\PseudoTypes\True_;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,7 +19,6 @@ class ContactController extends AbstractController
     public function index(Request $request): Response
     {
       
-        
         //creation du formulaire
         $form = $this->createForm(ContactType::class);
    
@@ -27,14 +27,11 @@ class ContactController extends AbstractController
         //condition du formulaire si tu es remplis et valid
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $notification = " Votre message a bien étè envoyé";
-           // $this->addFlash('notice', message: 'Merci de nous avoir contacter.Je vais vous répondre dansles meilleurs délais');
-            // vider le formulaire et le transmettre a la bdd
-           // $this->entityManager->flush();
+             $this->addFlash('notice', "Merci de nous avoir contacter, votre message a bien étè envoyé");
             // mail a reparemeter
            // $mail = new Mail();
            // $mail->send('lavoiedudragonidf@gmail.com', 'la voie du Dragon', 'vous avez reçu un nouveau contact', '');
-           // $this->addFlash('notice', "votre message a bien étè envoyé");
+           return $this->redirectToRoute('app_contact');
         }
             //envoyer formulaire sur le vue de twig
         return $this->render('contact/index.html.twig', [
