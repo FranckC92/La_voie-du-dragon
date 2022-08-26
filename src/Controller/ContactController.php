0543ab73c83473732ce43486e9c0ba5cf6776811
ceyssat <ceyssat.franck@gmail.com>
2022-08-26 13:57:34 +0200

    contact formulaire a vider et notification add falsh ou ça ne marche plus

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -13,33 +13,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
-    private $entityManager;
-
-    public function __construct(EntityManagerInterface $entitymanager)
-    {
-        $this->entityManager = $entitymanager;
-    }
     
     #[Route('/contact', name: 'app_contact')]
     public function index(Request $request): Response
     {
       
-       
+        
         //creation du formulaire
         $form = $this->createForm(ContactType::class);
    
         //handlerequest methode
         $form->handleRequest($request);
         //condition du formulaire si tu es remplis et valid
+
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->addFlash('notice', message: 'Merci de nous avoir contacter.Je vais vous répondre dansles meilleurs délais');
-           
+            $notification = " Votre message a bien étè envoyé";
+           // $this->addFlash('notice', message: 'Merci de nous avoir contacter.Je vais vous répondre dansles meilleurs délais');
             // vider le formulaire et le transmettre a la bdd
-            $this->entityManager->flush();
+           // $this->entityManager->flush();
             // mail a reparemeter
            // $mail = new Mail();
            // $mail->send('lavoiedudragonidf@gmail.com', 'la voie du Dragon', 'vous avez reçu un nouveau contact', '');
-            $this->addFlash('notice', 'votre message a bien étè envoyé');
+           // $this->addFlash('notice', "votre message a bien étè envoyé");
         }
             //envoyer formulaire sur le vue de twig
         return $this->render('contact/index.html.twig', [
